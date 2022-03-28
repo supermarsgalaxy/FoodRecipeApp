dd5317f88df8e2199da6a8ecda0bd2b58a1acce0
Mars Brown <meeshajean33@gmail.com>
2022-03-28 18:28:54 -0400

    fixed all errors

--- FoodRecipeApp.js
+++ FoodRecipeApp.js
@@ -1,35 +1,36 @@
-//globals
 const searchBtn = document.getElementById("search-btn");
 const mealList = document.getElementById("meal");
 const mealDetailsContent = document.querySelector(".meal-details-content");
 const recipeCloseBtn = document.getElementById("recipe-close-btn");
-//event listeners
+
+// event listeners
 searchBtn.addEventListener("click", getMealList);
 mealList.addEventListener("click", getMealRecipe);
 recipeCloseBtn.addEventListener("click", () => {
   mealDetailsContent.parentElement.classList.remove("showRecipe");
 });
-//get meal list that matches with the ingredients
-//i think this is where error is
+
+// get meal list that matches with the ingredients
 function getMealList() {
   let searchInputTxt = document.getElementById("search-input").value.trim();
   fetch(
-    "https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}"
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
   )
     .then((response) => response.json())
     .then((data) => {
+      let html = "";
       if (data.meals) {
         data.meals.forEach((meal) => {
           html += `
-                <div class="meal-item" data-id="${meal.idMeal}">
-                    <div class="meal-img">
-                        <img src = "${meal.strMealThumb}" alt="food"></img>
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
                     </div>
-                    <div class="meal-name">
-                        <h3>${meal.strMeal}</h3>
-                        <a href ="#" class="recipe-btn">Get Recipe</a>
-                    </div>
-                </div>
                 `;
         });
         mealList.classList.remove("notFound");
@@ -37,14 +38,16 @@ function getMealList() {
         html = "Sorry, we didn't find any meal!";
         mealList.classList.add("notFound");
       }
+
       mealList.innerHTML = html;
     });
 }
-//get recipe of the meal
-function getMealRecipe(event) {
-  event.preventDefault();
-  if (event.target.classList.contains("recipe-btn")) {
-    let mealItem = event.target.parentElement.parentElement;
+
+// get recipe of the meal
+function getMealRecipe(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("recipe-btn")) {
+    let mealItem = e.target.parentElement.parentElement;
     fetch(
       `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
     )
@@ -52,12 +55,13 @@ function getMealRecipe(event) {
       .then((data) => mealRecipeModal(data.meals));
   }
 }
-//create a modal
+
+// create a modal
 function mealRecipeModal(meal) {
   console.log(meal);
   meal = meal[0];
   let html = `
-   <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
         <p class = "recipe-category">${meal.strCategory}</p>
         <div class = "recipe-instruct">
             <h3>Instructions:</h3>
@@ -67,7 +71,7 @@ function mealRecipeModal(meal) {
             <img src = "${meal.strMealThumb}" alt = "">
         </div>
         <div class = "recipe-link">
-           <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
         </div>
     `;
   mealDetailsContent.innerHTML = html;
